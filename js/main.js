1cbfe76a4a7e89040165abf181b25568e87a4bf5
Adam Girzishev <girzishev99@bk.ru>
2023-02-11 04:22:03 +0300

    small
    small

---
+++ js/main.js
@@ -0,0 +1,46 @@
+//ПЕРВОЕ ЗАДАНИЕ
+
+/*let arr_1 = [101, 202, 303, 404, 505];
+let arr_2 = [606, 707, 808, 909];
+let arr_3 = [...arr_1, ...arr_2];
+console.log(arr_3);*/
+
+//ВТОРОЕ ЗАДАНИЕ
+
+//let superArr = [1, 5, 8, 9, 10];
+//console.log(Math.min(...superArr));
+
+//ТРЕТЬЕ ЗАДАНИЕ
+
+/*let obj = {
+  width: 300,
+  height: 550,
+};
+let obj2 = { ...obj };
+
+console.log(obj2.width * obj2.height);*/
+
+//ЧЕТВЕРТОЕ ЗАДАНИЕ
+
+/*function f(...b) {
+  console.log(b);
+  b.map((item, index) => {
+    console.log(item + item);
+  });
+}
+
+f(1, 5, 7, 8);*/
+
+//ПЯТОЕ ЗАДАНИЕ
+
+/*let num = 0;
+while (num < 3) {
+  console.log(`Число:${num}`);
+  num++;
+}*/
+
+//ШЕСТОЕ ЗАДАНИЕ
+
+/*for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}*/
